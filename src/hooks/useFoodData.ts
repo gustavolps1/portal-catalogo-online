e3024591c3fefdfbc432ad901818620feeec5e2b
Gustavo Lopes <gustavolopes.dev@gmail.com>
2023-12-01 21:48:01 -0300

    api produto url fix

--- src/hooks/useFoodData.ts
+++ src/hooks/useFoodData.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosPromise } from "axios"
 import { FoodData } from '../interface/FoodData';
 
-const API_URL = 'http://portal-catalogo-online-qqlmpck1m-gustavolps1s-projects.vercel.app';
+const API_URL = 'http://api-produto-pitsi2-production.up.railway.app';
 
 const fetchData = async (): AxiosPromise<FoodData[]> => {
     const response = axios.get(API_URL + '/produto');
--- src/hooks/useFoodDataMutate.ts
+++ src/hooks/useFoodDataMutate.ts
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios, { AxiosPromise } from "axios"
 import { FoodData } from '../interface/FoodData';
 
-const API_URL = 'http://portal-catalogo-online-qqlmpck1m-gustavolps1s-projects.vercel.app';
+const API_URL = 'http://api-produto-pitsi2-production.up.railway.app';
 
 const postData = async (data: FoodData[]): AxiosPromise<any> => {
     const response = axios.post(API_URL + '/produto', data);
